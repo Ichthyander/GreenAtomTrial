8247bf7f43ceb4e3ddc62afa8a63152fa8b13e0a
di_ignatov@mail.ru <di_ignatov@mail.ru>
2023-09-25 23:14:12 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using GreenAtomTrial.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenAtomTrial
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var mainWindow = new MainView(commandData);
+            mainWindow.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
